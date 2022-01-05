94487dc041d226685bad105a56187a940f56e3e6
shanerodrigues <shane2010r@hotmail.com>
2022-01-05 17:08:16 +1100

    committing build to heroku

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const cors = require("cors")
 
 app.use(express.json())
 app.use(cors())
+app.use(express.static("build"))
 
 morgan.token("personInfo", function (req, res) {
   return JSON.stringify(req.body)
